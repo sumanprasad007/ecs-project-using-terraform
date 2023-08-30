c90136fa385b33e8a2c870501d1db7b6aff40dea
Suman Prasad <sumanprasad9766@gmail.com>
2023-08-31 00:25:11 +0530

    first initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var express = require('express')
+var app = express()
+
+app.get('/', function(req, res){
+  res.send('Hello this is Ashutosh! I am 20 years old and I am a student. I studing B.tech in Cloud Technology');
+});
+
+/* istanbul ignore next */
+if (!module.parent) {
+  app.listen(3000);
+  console.log('Express started on port 3000');
+}
