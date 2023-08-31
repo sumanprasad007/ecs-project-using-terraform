fe22f7c19a42699a63c233232e04a4ff6b80fef6
Suman Prasad <sumanprasad9766@gmail.com>
2023-08-31 12:45:17 +0530

    modified the index file

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function(req, res){
-  res.send('Hello this is Ashutosh! I am 20 years old and I am a student. I studing B.tech in Cloud Technology');
-});
+  res.send('Hello Everyone, hope you are doing well. This is a Node Express application runnng on port 3000. This application is backed with GitHub Action as CICD Pipeline, which continously monitors and updates the changes to the server')});
 
 /* istanbul ignore next */
 if (!module.parent) {
